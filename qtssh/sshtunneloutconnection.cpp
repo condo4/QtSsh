a1ebd39feb5ccb62889cc0aa5d39b17fdb32c582
Fabien Proriol <fabien.proriol@viavisolutions.com>
2020-01-09 14:46:27 +0100

    tunneloutconnection: After error, need to be free

--- qtssh/sshtunneloutconnection.cpp
+++ qtssh/sshtunneloutconnection.cpp
@@ -179,6 +179,7 @@ void SshTunnelOutConnection::_eventLoop()
         case Error:
         {
             qCDebug(logsshtunneloutconnection) << "Channel" << m_name << "is in error state";
+            setChannelState(Free);
             return;
         }
     }
